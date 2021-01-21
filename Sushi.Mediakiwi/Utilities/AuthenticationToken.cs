7fc0e90915c3943672b1bebb9848f7fbaf949dcf
Build78 <marc@build78.nl>
2021-01-21 14:34:51 +0100

    cleaned the authentication token in combination with easyAuth

---
+++ Sushi.Mediakiwi/Utilities/AuthenticationToken.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sushi.Mediakiwi.Utilities
+{
+    public class AuthenticationToken
+    {
+        public string id_token
+        {
+            get; set;
+        }
+        public string provider_name
+        {
+            get; set;
+        }
+        public List<UserClaim> user_claims
+        {
+            get; set;
+        }
+        public string user_id
+        {
+            get; set;
+        }
+    }
+
+    public class UserClaim
+    {
+        public string typ
+        {
+            get; set;
+        }
+        public string val
+        {
+            get; set;
+        }
+    }
+}
