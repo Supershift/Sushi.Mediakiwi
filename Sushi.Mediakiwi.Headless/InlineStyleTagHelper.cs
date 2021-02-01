907b78a163ec9acd62ca4c7d9f0caf0e9b2a617f
Mark Rienstra <iemand38@hotmail.com>
2021-02-01 14:04:55 +0100

    Added taghelper to MediaKiwi

---
+++ Sushi.Mediakiwi.Headless/InlineStyleTagHelper.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Sushi.Mediakiwi.Headless
+{
+    /// <summary>
+    /// Reads the given File from the Href attribute and emits its output inside a style tag
+    /// </summary>
+    public class InlineStyleTagHelper : TagHelper
+    {
+        public InlineStyleTagHelper(IWebHostEnvironment webHostEnvironment, IMemoryCache cache)
+        {
+            WebHostEnvironment = webHostEnvironment;
+            Cache = cache;
+        }
+
+        [HtmlAttributeName("href")]
+        public string Href { get; set; }
+
+        private IWebHostEnvironment WebHostEnvironment { get; }
+        private IMemoryCache Cache { get; }
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var path = Href;
+            var version = string.Empty;
+
+            if (Href.IndexOf('?') > -1)
+            {
+                version = Href.Substring(Href.IndexOf('?'));
+                path = path.Remove(Href.IndexOf('?'));
+            }
+
+            // Get the value from the cache, or compute the value and add it to the cache
+            var fileContent = await Cache.GetOrCreateAsync($"InlineStyleTagHelper-{path}-{version}", async entry => {
+                IFileProvider fileProvider = WebHostEnvironment.WebRootFileProvider;
+                IChangeToken changeToken = fileProvider.Watch(path);
+
+                entry.SetPriority(CacheItemPriority.NeverRemove);
+                entry.AddExpirationToken(changeToken);
+
+                IFileInfo file = fileProvider.GetFileInfo(path);
+                if (file == null || !file.Exists)
+                    return null;
+
+                return await ReadFileContent(file);
+            });
+
+            if (fileContent == null)
+            {
+                output.SuppressOutput();
+                return;
+            }
+
+            output.TagName = "style";
+            output.Attributes.RemoveAll("href");
+            output.PostContent.AppendHtml(fileContent);
+        }
+
+
+        private static async Task<string> ReadFileContent(IFileInfo file)
+        {
+            using var stream = file.CreateReadStream();
+            using var textReader = new StreamReader(stream);
+
+            return await textReader.ReadToEndAsync();
+        }
+    }
+}
