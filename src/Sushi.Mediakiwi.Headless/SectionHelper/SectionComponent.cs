b588e73860c296ae0e6a5253f381c17963ee59b1
Mark Rienstra <iemand38@hotmail.com>
2021-02-10 16:18:20 +0100

    Included the SectionServiceLib library (https://github.com/inspgadget/BlazorSections) with some much needed fixes

---
+++ src/Sushi.Mediakiwi.Headless/SectionHelper/SectionComponent.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Sushi.Mediakiwi.Headless.SectionHelper.SectionService;
+
+namespace Sushi.Mediakiwi.Headless.SectionHelper
+{
+    public class SectionComponent : ComponentBase, IDisposable
+    {
+        [Inject]
+        public SectionService Service { get; set; }
+
+        [Parameter]
+        public string SectionName { get; set; }
+
+        private bool _initialised;
+        private ISection _section;
+        private int _sequence;
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            if (!_initialised)
+            {
+                if (string.IsNullOrWhiteSpace(SectionName))
+                {
+                    throw new ArgumentOutOfRangeException("SectionName must be set.");
+                }
+                _section = Service.RegisterSection(SectionName);
+                _section.ChangesDone += Service_ChangesDone;
+                _initialised = true;
+            }
+
+            int sequence = 0;
+            List<Element> elements = _initialised ? _section.Elements.Where(x => !x.ShouldUpdate).ToList() : _section.Elements;
+            foreach (Element element in elements)
+            {
+                BuildElement(builder, element, ref sequence);
+            }
+            sequence = _sequence;
+            if (_initialised)
+            {
+                foreach (Element element in _section.Elements.Where(x => x.ShouldUpdate || x.Sequence == -1))
+                {
+                    BuildElement(builder, element, ref sequence);
+                    sequence++;
+                }
+                _sequence = sequence;
+            }
+            _section.Elements.Where(x => x.ShouldUpdate = true).ToList().ForEach(x => x.ShouldUpdate = false);
+        }
+
+        private void BuildElement(RenderTreeBuilder builder, Element element, ref int sequence)
+        {
+            if (element.Sequence == -1)
+            {
+                element.Sequence = sequence;
+            }
+            else if (element.ShouldUpdate)
+            {
+                sequence = element.Sequence;
+            }
+            builder.OpenElement(sequence, element.Name);
+            sequence++;
+            foreach (var kv in element.AllProperties)
+            {
+                builder.AddAttribute(sequence, kv.Key, kv.Value);
+                sequence++;
+            }
+            if (string.IsNullOrWhiteSpace(element?.Content.Value) == false)
+            {
+                builder.AddContent(sequence, element.Content);
+                sequence++;
+            }
+            builder.CloseElement();
+        }
+
+        protected async void Service_ChangesDone(object sender, EventArgs e)
+        {
+            await InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+        }
+
+        public void Dispose()
+        {
+            Service = null;
+            _section.ChangesDone -= Service_ChangesDone;
+            _section = null;
+        }
+    }
+}
---
+++ src/Sushi.Mediakiwi.Headless/SectionHelper/SectionService.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sushi.Mediakiwi.Headless.SectionHelper
+{
+
+    public static class SectionServiceExtensions
+    {
+        public static void AddSectionHelper(this IServiceCollection services)
+        {
+            services.AddScoped<SectionService>();
+        }
+    }
+
+    public class SectionService : IDisposable
+    {
+        public class Element
+        {
+            public string Name { get; }
+            public MarkupString Content { get; protected set; }
+            public int Sequence { get; set; } = -1;
+            public bool ShouldUpdate { get; set; }
+            protected Dictionary<string, string> Properties { get; } = new Dictionary<string, string>();
+            protected Dictionary<string, string> PrivateProperties { get; } = new Dictionary<string, string>();
+
+            public void AddProperty(string name, string value)
+            {
+                Properties.Add(name, value);
+                ShouldUpdate = true;
+            }
+
+            public void RemoveProperty(string name)
+            {
+                Properties.Remove(name);
+                ShouldUpdate = true;
+            }
+
+            public Dictionary<string, string> AllProperties
+            {
+                get
+                {
+                    Dictionary<string, string> d = new Dictionary<string, string>();
+                    foreach (var kv in PrivateProperties)
+                    {
+                        d.Add(kv.Key, kv.Value);
+                    }
+                    foreach (var kv in Properties)
+                    {
+                        d.Add(kv.Key, kv.Value);
+                    }
+                    return d;
+                }
+            }
+
+            public Element(string name) : this(name, string.Empty) { }
+
+            public Element(string name, string content)
+            {
+                Name = name;
+                Content = new MarkupString(content);
+            }
+
+            public Element(string name, MarkupString content)
+            {
+                Name = name;
+                Content = content;
+            }
+        }
+
+        public class Javascript : Element
+        {
+            public string Uri
+            {
+                get
+                {
+                    return PrivateProperties["src"];
+                }
+            }
+
+            public Javascript(string uri) : base("script", null)
+            {
+                PrivateProperties.Add("type", "text/javascript");
+                PrivateProperties.Add("src", uri);
+            }
+        }
+
+        public class Stylesheet : Element
+        {
+            public string Uri
+            {
+                get
+                {
+                    return PrivateProperties.ContainsKey("href") ? PrivateProperties["href"] : null;
+                }
+            }
+
+            public Stylesheet(string uri) : base("link")
+            {
+                PrivateProperties.Add("rel", "stylesheet");
+                PrivateProperties.Add("type", "text/css");
+                PrivateProperties.Add("href", uri);
+            }
+        }
+
+        public class InlineJavaScript : Element
+        {
+            public InlineJavaScript(string content) : base("script", content)
+            {
+                PrivateProperties.Add("type", "text/javascript");
+            }
+
+            public InlineJavaScript(MarkupString content) : base("script", content)
+            {
+                PrivateProperties.Add("type", "text/javascript");
+            }
+        }
+
+        public class InlineStylesheet : Element
+        {
+            public InlineStylesheet(string content) : base("style", content)
+            {
+                PrivateProperties.Add("type", "text/css");
+            }
+
+            public InlineStylesheet(MarkupString content) : base("style", content)
+            {
+                PrivateProperties.Add("type", "text/css");
+            }
+        }
+
+        private string _lastUri;
+        private List<Section> _sections = new List<Section>();
+        private NavigationManager _uriHelper;
+
+        public SectionService(NavigationManager uriHelper)
+        {
+            _uriHelper = uriHelper;
+            _lastUri = _uriHelper.Uri;
+            _uriHelper.LocationChanged += UriHelper_LocationChanged;
+        }
+
+        public void AddElement(string sectionName, Element element)
+        {
+            Section section = _sections.FirstOrDefault(x => x.Name == sectionName);
+            if (section == null)
+            {
+                section = new Section(sectionName);
+                _sections.Add(section);
+            }
+            section.Elements.Add(element);
+            section.InvokeChangesDone();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            _uriHelper.LocationChanged -= UriHelper_LocationChanged;
+            foreach (Section s in _sections)
+            {
+                s.Dispose();
+            }
+            _sections = null;
+            _uriHelper = null;
+        }
+
+        ~SectionService()
+        {
+            Dispose(false);
+        }
+
+        public ISection RegisterSection(string sectionName)
+        {
+            Section section = _sections.FirstOrDefault(x => x.Name == sectionName);
+            if (section == null)
+            {
+                section = new Section(sectionName);
+                _sections.Add(section);
+
+            }
+            return section;
+        }
+
+        private void UriHelper_LocationChanged(object sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
+        {
+            if (_lastUri != e.Location)
+            {
+                _lastUri = e.Location;
+                foreach (Section s in _sections)
+                {
+                    s.Elements.Clear();
+                    s.InvokeChangesDone();
+                }
+            }
+            else
+            {
+                foreach (Section s in _sections)
+                {
+                    s.Elements.Where(x => x is InlineJavaScript).ToList().ForEach(x => { x.ShouldUpdate = true; x.Sequence = -1; });
+                    s.InvokeChangesDone();
+                }
+            }
+        }
+
+        public interface ISection
+        {
+            string Name { get; }
+
+            event EventHandler<EventArgs> ChangesDone;
+
+            List<Element> Elements { get; }
+        }
+
+        public class Section : ISection, IDisposable
+        {
+            public string Name { get; private set; }
+
+            public event EventHandler<EventArgs> ChangesDone;
+
+            public Section(string name)
+            {
+                Name = name;
+            }
+
+            public List<Element> Elements { get; private set; } = new List<Element>();
+
+            public void InvokeChangesDone()
+            {
+                ChangesDone?.Invoke(this, EventArgs.Empty);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                Elements = null;
+                ChangesDone = null;
+            }
+
+            ~Section()
+            {
+                Dispose(false);
+            }
+        }
+    }
+}
