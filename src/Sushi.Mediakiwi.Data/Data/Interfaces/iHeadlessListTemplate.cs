f951ae6aad2fa516472e55eae0757cc55fb3dbb3
MMolenwijk <marc.molenwijk@supershift.nl>
2021-02-26 09:30:30 +0100

    headless version

---
+++ src/Sushi.Mediakiwi.Data/Data/Interfaces/iHeadlessListTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sushi.Mediakiwi.Data
+{
+    public interface iHeadlessListTemplate
+    {
+        /// <summary>
+        /// Get the element for headless use
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        void DoHeadLessFetch(HeadlessRequest request);
+    }
+}
