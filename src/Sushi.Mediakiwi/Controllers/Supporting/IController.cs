1ca7b0c94ae39b79bfbca2d528c0b989ad82a92c
marcmolenwijk <53566220+marcmolenwijk@users.noreply.github.com>
2021-07-15 10:10:43 +0200

    added "removed" base values

--- src/Sushi.Mediakiwi/Controllers/Supporting/IController.cs
+++ src/Sushi.Mediakiwi/Controllers/Supporting/IController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Sushi.Mediakiwi.Data;
 using System.Threading.Tasks;
 
 namespace Sushi.Mediakiwi.Controllers
 {
     public interface IController
     {
+        IApplicationUser CurrentApplicationUser { get; }
+        IVisitor CurrentVisitor { get; }
     }
 }
