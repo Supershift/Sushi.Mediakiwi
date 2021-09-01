465cfa1a289e8d1e88b05aeb05ed4172988fb37a
Mark de Vries <mark.devries@supershift.nl>
2021-09-01 14:57:14 +0200

    exclude path

---
+++ src/Sushi.Mediakiwi.Demonstration/Controllers/CustomController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sushi.Mediakiwi.Demonstration.Controllers
+{
+    [ApiController]
+    public class CustomController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/custom/ping")]        
+        public ActionResult Ping()
+        {
+            return Ok("hello world");
+        }
+
+        [HttpGet]
+        [Route("api/custom/resource/{id}")]
+        public ActionResult GetResource(string id)
+        {
+            return Ok($"Resource id: {id}");
+        }
+    }
+}
