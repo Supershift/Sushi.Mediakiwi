5eb64ffa331f53c4a3778306b59d98db1e17da35
Mark Rienstra <iemand38@hotmail.com>
2021-12-07 15:01:04 +0100

    CORS policy for API adjusted Cookie policy for API adjusted

---
+++ src/Sushi.Mediakiwi.API/Middleware/ApiCorsMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Sushi.Mediakiwi.API.Middleware
+{
+    public class ApiCorsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ApiCorsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date");
+            return _next(httpContext);
+        }
+    }
+}
