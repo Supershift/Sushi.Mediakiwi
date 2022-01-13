4d1705eeb523ca3ff3428e277165cc2bb7fc4f3f
Mark Rienstra <iemand38@hotmail.com>
2022-01-13 09:50:53 +0100

    Added mkapi authentication check to auth handler

--- src/Sushi.Mediakiwi/Authentication/MediaKiwiAuthenticationHandler.cs
+++ src/Sushi.Mediakiwi/Authentication/MediaKiwiAuthenticationHandler.cs
@@ -5,7 +5,7 @@ using Sushi.Mediakiwi.Data;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
-using System.Text;
+using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
@@ -25,6 +25,28 @@ namespace Sushi.Mediakiwi.Authentication
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
+            // If we land here from an API call where a MKAPI call has been made first,
+            // we can identify the user based on it's GUID claim
+            if (Context?.User?.HasClaim(x => x.Type == "guid")== true)
+            {
+                var userGuid = Guid.Parse(Context.User.Claims.FirstOrDefault(x => x.Type == "guid").Value);
+
+                var currentApplicationUser = await ApplicationUser.SelectOneAsync(userGuid).ConfigureAwait(false);
+                if (currentApplicationUser?.ID > 0 && currentApplicationUser.IsActive == true)
+                {
+                    var principal = new ClaimsPrincipal(Context.User.Identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                    return AuthenticateResult.Success(ticket);
+                }
+                else
+                {
+                    return AuthenticateResult.Fail("Invalid user");
+                }
+            }
+
+            // If we land here directly from the Portal, we should have a visitor
+            // Containing user information
             var visManager = new VisitorManager(Context);
 
             var currentVisitor = await visManager.SelectAsync().ConfigureAwait(false);
