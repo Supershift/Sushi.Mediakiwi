09cbb018a30371eff0f0ae1d57568f58baf1a5ee
Bart van der Drift <bart.vanderdrift@supershift.nl>
2022-03-10 09:40:20 +0100

    7.17.1

--- src/Sushi.Mediakiwi/Logic/OAuth2Logic.cs
+++ src/Sushi.Mediakiwi/Logic/OAuth2Logic.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using System.Security.Cryptography;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
 
 namespace Sushi.Mediakiwi.Logic
 {
@@ -18,12 +23,12 @@ namespace Sushi.Mediakiwi.Logic
             if (WimServerConfiguration.Instance.Authentication != null && WimServerConfiguration.Instance.Authentication.Aad != null && WimServerConfiguration.Instance.Authentication.Aad.Enabled)
             {
                 var callback = WimServerConfiguration.Instance.Authentication.Aad.RedirectUrl;
-                
+
                 if (callback.OriginalString.StartsWith("/", StringComparison.OrdinalIgnoreCase))
                 {
                     callback = new Uri($"{domain}{callback}");
                 }
-                
+
                 var ad_clientid = WimServerConfiguration.Instance.Authentication.Aad.Client;
                 var ad_tenantid = WimServerConfiguration.Instance.Authentication.Aad.Tenant;
 
@@ -34,21 +39,45 @@ namespace Sushi.Mediakiwi.Logic
         }
 
         private static Regex EmailRegex { get; set; } = new Regex(Data.Utility.GlobalRegularExpression.EmailAddress, RegexOptions.IgnoreCase);
-        public static async Task<string> ExtractUpnAsync(TokenValidation validation, string idtoken)
+        public static async Task<string> ExtractUpnAsync(AuthenticationConfiguration authenticationConfiguration, string idtoken, HttpContext context)
         {
-            if (IsValidToken(validation, idtoken))
+            try
             {
-                try
-                {
-                    var handler = new JwtSecurityTokenHandler();
-                    var jsonToken = handler.ReadToken(idtoken);
-                    var token = jsonToken as JwtSecurityToken;
+                var handler = new JsonWebTokenHandler();
+                var jsonToken = handler.ReadToken(idtoken);
+                var token = jsonToken as JsonWebToken;
 
-                    if (token == null)
+                if (token == null)
+                {
+                    if (authenticationConfiguration.Aad?.LogUpnExtractionErrors == true)
                     {
                         await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Error, $"No token encountered: {idtoken}").ConfigureAwait(false);
-                        return null;
                     }
+                    return null;
+                }
+
+                // todo: make this an instance method and provide configurationmanager through DI                
+                var openIdConfigurationManager = context.RequestServices.GetRequiredService<ConfigurationManager<OpenIdConnectConfiguration>>();
+                var openIdConnectConfigData = await openIdConfigurationManager.GetConfigurationAsync();
+
+                var validationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    RequireSignedTokens = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidAudience = authenticationConfiguration.Aad.Client,
+                    IssuerSigningKeys = openIdConnectConfigData.SigningKeys,
+                    ValidIssuer = openIdConnectConfigData.Issuer,
+                };
+
+
+                var isValidToken = handler.ValidateToken(idtoken, validationParameters);
+
+                if (isValidToken.IsValid)
+                {
 
                     const string DEFAULT_CLAIM_TYPE = "email";
                     List<string> claimtypes = new List<string>();
@@ -62,7 +91,10 @@ namespace Sushi.Mediakiwi.Logic
                     }
                     catch (Exception ex)
                     {
-                        await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), ex).ConfigureAwait(false);
+                        if (authenticationConfiguration.Aad?.LogUpnExtractionErrors == true)
+                        {
+                            await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), ex).ConfigureAwait(false);
+                        }
                     }
 
                     // BD 2021-08-05: Loop through all the claims and look for the email
@@ -73,7 +105,7 @@ namespace Sushi.Mediakiwi.Logic
                         if (claimtypes.Any(x => x.Equals(claim.Type, StringComparison.CurrentCultureIgnoreCase)))
                         {
                             // If we find the claim with the configured type, return it immediately
-                            return await LogAndReturnAsync(claim.Value).ConfigureAwait(false);
+                            return await LogAndReturnAsync(authenticationConfiguration, claim.Value).ConfigureAwait(false);
                         }
                         else if (claim.Type.Equals(DEFAULT_CLAIM_TYPE, StringComparison.CurrentCultureIgnoreCase))
                         {
@@ -90,140 +122,50 @@ namespace Sushi.Mediakiwi.Logic
                     // If we have not found the claim of the configured type, return the claim of the default type if we found it
                     if (claimOfDefaultType != null)
                     {
-                        return await LogAndReturnAsync(claimOfDefaultType.Value).ConfigureAwait(false);
+                        return await LogAndReturnAsync(authenticationConfiguration, claimOfDefaultType.Value).ConfigureAwait(false);
                     }
 
                     // Otherwise, if we found a claim that has the form of an email address, return that
                     if (claimWithEmailValue != null)
                     {
-                        return await LogAndReturnAsync(claimWithEmailValue.Value).ConfigureAwait(false);
+                        return await LogAndReturnAsync(authenticationConfiguration, claimWithEmailValue.Value).ConfigureAwait(false);
                     }
 
                     // If we could not find a claim of an expected type, but we only have one claim, return that
                     if (token.Claims.Count() == 1)
                     {
-                        return await LogAndReturnAsync(token.Claims.First().Value).ConfigureAwait(false);
+                        return await LogAndReturnAsync(authenticationConfiguration, token.Claims.First().Value).ConfigureAwait(false);
                     }
 
                     // We could not find a claim. Log the token for examination.
-                    await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Error, $"No email claim found: {idtoken}").ConfigureAwait(false);
+                    if (authenticationConfiguration.Aad?.LogUpnExtractionErrors == true)
+                    {
+                        await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Error, $"No email claim found: {idtoken}").ConfigureAwait(false);
+                    }
                 }
-                catch (Exception ex)
+                else if (authenticationConfiguration.Aad?.LogUpnExtractionErrors == true)
                 {
-                    await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), ex).ConfigureAwait(false);
-                    return null;
+                    await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Error, $"Token not valid: {idtoken}, {isValidToken.Exception}").ConfigureAwait(false);
                 }
             }
-            else
+            catch (Exception ex)
             {
-                await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Error, $"Token not valid: {idtoken}").ConfigureAwait(false);
+                if (authenticationConfiguration.Aad?.LogUpnExtractionErrors == true)
+                {
+                    await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), ex).ConfigureAwait(false);
+                }
+                return null;
             }
             return null;
         }
 
-        private static async Task<string> LogAndReturnAsync(string email)
+        private static async Task<string> LogAndReturnAsync(AuthenticationConfiguration authenticationConfiguration, string email)
         {
-            await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Information, $"Extracted: {email}").ConfigureAwait(false);
+            if (authenticationConfiguration.Aad?.LogUpnExtraction == true)
+            {
+                await Data.Notification.InsertOneAsync(nameof(ExtractUpnAsync), Data.NotificationType.Information, $"Extracted: {email}").ConfigureAwait(false);
+            }
             return email;
-        }
-
-        public static bool IsValidToken(TokenValidation validation, string idtoken)
-        {
-            if (string.IsNullOrWhiteSpace(idtoken))
-            {
-                throw new ArgumentNullException(nameof(idtoken));
-            }
-            if (validation == null)
-            {
-                throw new ArgumentNullException(nameof(validation));
-            }
-
-            var exponent = validation.Exponent;
-            var modulus = validation.Modulus;
-            return VerifyToken(idtoken, exponent, modulus);
-        }
-
-        private static bool VerifyToken(string idToken, string exponent, string modulus)
-        {
-            try
-            {
-                var parts = idToken.Split('.');
-                var header = parts[0];
-                var payload = parts[1];
-                string signedSignature = parts[2];
-                //Extract user info from payload   
-                string userInfo = Encoding.UTF8.GetString(Base64UrlDecode(payload));
-                //Which will be Verified
-                string originalMessage = string.Concat(header, ".", payload);
-                byte[] keyBytes = Base64UrlDecode(modulus);
-                string keyBase = Convert.ToBase64String(keyBytes);
-                string key = @"<RSAKeyValue> <Modulus>" + keyBase + "</Modulus> <Exponent>" + exponent + "</Exponent> </RSAKeyValue>";
-                bool result = VerifyData(originalMessage, signedSignature, key);
-                if (result)
-                    return true;
-                else
-                    return false;
-            }
-            catch (Exception) 
-            {
-                // to assure no exception being thrown
-            }
-            return false;
-        }
-
-        /// <summary>  
-        /// Verifies encrypted signed message with public key encrypted original message.  
-        /// </summary>  
-        /// <param name="originalMessage">Original message as string. (Encrypted form)</param>  
-        /// <param name="signedMessage">Signed message as string. (Encrypted form)</param>  
-        /// <param name="publicKey">Public key as XML string.</param>  
-        /// <returns>Boolean True if successful otherwise return false.</returns>  
-        private static bool VerifyData(string originalMessage, string signedMessage, string publicKey)
-        {
-            bool success = false;
-            using (var rsa = new RSACryptoServiceProvider())
-            {
-                var encoder = new UTF8Encoding();
-                byte[] bytesToVerify = encoder.GetBytes(originalMessage);
-                byte[] signedBytes = Base64UrlDecode(signedMessage);
-                try
-                {
-
-                    rsa.FromXmlString(publicKey);
-                    SHA256Managed Hash = new SHA256Managed();
-                    byte[] hashedData = Hash.ComputeHash(signedBytes);
-                    // Summary:
-                    //     Verifies that a digital signature is valid by determining the hash value in the
-                    //     signature using the provided public key and comparing it to the hash value of
-                    //     the provided data.
-                    success = rsa.VerifyData(bytesToVerify, CryptoConfig.MapNameToOID("SHA256"), signedBytes);
-                }
-                catch (CryptographicException e)
-                {
-                    success = false;
-                }
-                finally
-                {
-                    rsa.PersistKeyInCsp = false;
-                }
-            }
-            return success;
-        }
-
-        private static byte[] Base64UrlDecode(string input)
-        {
-            var output = input;
-            output = output.Replace('-', '+'); // 62nd char of encoding  
-            output = output.Replace('_', '/'); // 63rd char of encoding  
-            switch (output.Length % 4) // Pad with trailing '='s  
-            {
-                case 0: break; // No pad chars in this case  
-                case 2: output += "=="; break; // Two pad chars  
-                case 3: output += "="; break; // One pad char  
-                default: throw new System.Exception("Illegal base64url string!");
-            }
-            var converted = Convert.FromBase64String(output); // Standard base64 decoder  
-            return converted;
         }
     }
 }
